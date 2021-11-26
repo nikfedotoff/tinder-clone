7db1016e7cc511dc13fefe21118d6e8bb864db9b
Nikita Fedotov <nikfedotoff@mail.ru>
2021-11-27 02:29:55 +0600

    navigation added

---
+++ screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import { useNavigation } from '@react-navigation/core'
+import React from 'react'
+import { View, Text, Button } from 'react-native'
+
+const HomeScreen = () => {
+    const navigation = useNavigation()
+
+    return (
+        <View>
+            <Text>Home page</Text>
+            <Button
+                title="Go to chat screen"
+                onPress={() => navigation.navigate('Chat')}
+            />
+        </View>
+    )
+}
+
+export default HomeScreen
